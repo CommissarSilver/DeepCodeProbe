2e35da4fc82048d36237a54e3f63d72530ddad18
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-05-15 11:58:52 -0400

    who knows

--- src/ast-probe/data/data_loading.py
+++ src/ast-probe/data/data_loading.py
@@ -1,4 +1,4 @@
-import logging
+import os, logging
 
 from collections import Counter
 from tree_sitter import Language, Parser
@@ -7,10 +7,10 @@ from .binary_tree import ast2binary, tree_to_distance
 from .code2ast import code2ast, get_tokens_ast
 
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("data")
 
-PY_LANGUAGE = Language("grammars/languages.so", "python")
-JAVA_LANGUAGE = Language("grammars/languages.so", "java")
+PY_LANGUAGE = Language("src/ast-probe/grammars/languages.so", "python")
+JAVA_LANGUAGE = Language("src/ast-probe/grammars/languages.so", "java")
 
 PY_PARSER = Parser()
 PY_PARSER.set_language(PY_LANGUAGE)
@@ -19,10 +19,19 @@ JAVA_PARSER.set_language(JAVA_LANGUAGE)
 
 
 def convert_sample_to_features(code, parser, lang):
-    G, pre_code = code2ast(code, parser, lang)
-    binary_ast = ast2binary(G)
-    d, c, _, u = tree_to_distance(binary_ast, 0)
-    code_tokens = get_tokens_ast(G, pre_code)
+    try:
+        G, pre_code = code2ast(code, parser, lang)
+        binary_ast = ast2binary(G)
+        d, c, _, u = tree_to_distance(binary_ast, 0)
+        code_tokens = get_tokens_ast(G, pre_code)
+
+        logger.info(f"Successfully converted sample to features")
+        logger.debug(
+            f"Input code language: ( {lang} ) - code: ( {code} ) - (d,c,u) = ( {d}, {c}, {u} )"
+        )
+    except Exception as e:
+        logger.exception(f"Error while converting sample to features: {e}")
+        d = [], c = [], u = [], code_tokens = []
 
     return {
         "d": d,
--- src/ast-probe/probe/loss.py
+++ src/ast-probe/probe/loss.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class ParserLoss(nn.Module):
-    def __init__(self, loss='l1', pretrained=False):
+    def __init__(self, loss="l1", pretrained=False):
         super(ParserLoss, self).__init__()
         self.cs = nn.CrossEntropyLoss(ignore_index=-1)
         self.loss = loss
@@ -14,19 +14,29 @@ class ParserLoss(nn.Module):
         labels_1s = (d_real != -1).float()
         d_pred_masked = d_pred * labels_1s  # b x seq-1
         d_real_masked = d_real * labels_1s  # b x seq-1
-        if self.loss == 'l1':
-            loss_d = torch.sum(torch.abs(d_pred_masked - d_real_masked), dim=1) / (length_batch.float() - 1)
+        
+        if self.loss == "l1":
+            loss_d = torch.sum(torch.abs(d_pred_masked - d_real_masked), dim=1) / (
+                length_batch.float() - 1
+            )
             loss_d = torch.sum(loss_d) / total_sents
-        elif self.loss == 'l2':
-            loss_d = torch.sum(torch.abs(d_pred_masked - d_real_masked) ** 2, dim=1) / (length_batch.float() - 1)
+        elif self.loss == "l2":
+            loss_d = torch.sum(torch.abs(d_pred_masked - d_real_masked) ** 2, dim=1) / (
+                length_batch.float() - 1
+            )
             loss_d = torch.sum(loss_d) / total_sents
-        elif self.loss == 'rank':
+        elif self.loss == "rank":
             lens_d = length_batch - 1
             max_len_d = torch.max(lens_d)
-            mask = torch.arange(max_len_d, device=max_len_d.device)[None, :] < lens_d[:, None]
+            mask = (
+                torch.arange(max_len_d, device=max_len_d.device)[None, :]
+                < lens_d[:, None]
+            )
             loss_d = rankloss(d_pred, d_real, mask, exp=False)
+        
         loss_c = self.cs(scores_c.view(-1, scores_c.shape[2]), c_real.view(-1))
         loss_u = self.cs(scores_u.view(-1, scores_u.shape[2]), u_real.view(-1))
+        
         if self.pretrained:
             return loss_c + loss_u
         else:
