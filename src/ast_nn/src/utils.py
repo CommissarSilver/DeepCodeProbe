655823daaaef3b0d0a11d264c730dec6e88e1bbb
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-05-22 16:59:32 -0400

    ast_nn utils changes: - fixed the import problem with try/except - formatting

--- src/ast_nn/src/utils.py
+++ src/ast_nn/src/utils.py
@@ -1,16 +1,22 @@
 import pandas as pd
 import javalang
 from javalang.ast import Node
-from tree import ASTNode, BlockNode
+
+try:
+    from tree import ASTNode, BlockNode
+except ModuleNotFoundError:
+    from ast_nn.src.tree import ASTNode, BlockNode
 import sys
+
 sys.setrecursionlimit(10000)
 
+
 def get_token(node):
-    token = ''
+    token = ""
     if isinstance(node, str):
         token = node
     elif isinstance(node, set):
-        token = 'Modifier'#node.pop()
+        token = "Modifier"  # node.pop()
     elif isinstance(node, Node):
         token = node.__class__.__name__
 
@@ -35,6 +41,7 @@ def get_children(root):
 
     return list(expand(children))
 
+
 def get_sequence(node, sequence):
     token, children = get_token(node), get_children(node)
     sequence.append(token)
@@ -42,18 +49,30 @@ def get_sequence(node, sequence):
     for child in children:
         get_sequence(child, sequence)
 
-    if token in ['ForStatement', 'WhileStatement', 'DoStatement','SwitchStatement', 'IfStatement']:
-        sequence.append('End')
+    if token in [
+        "ForStatement",
+        "WhileStatement",
+        "DoStatement",
+        "SwitchStatement",
+        "IfStatement",
+    ]:
+        sequence.append("End")
 
 
 def get_blocks_v1(node, block_seq):
     name, children = get_token(node), get_children(node)
-    logic = ['SwitchStatement','IfStatement', 'ForStatement', 'WhileStatement', 'DoStatement']
-    if name in ['MethodDeclaration', 'ConstructorDeclaration']:
+    logic = [
+        "SwitchStatement",
+        "IfStatement",
+        "ForStatement",
+        "WhileStatement",
+        "DoStatement",
+    ]
+    if name in ["MethodDeclaration", "ConstructorDeclaration"]:
         block_seq.append(BlockNode(node))
         body = node.body
         for child in body:
-            if get_token(child) not in logic and not hasattr(child, 'block'):
+            if get_token(child) not in logic and not hasattr(child, "block"):
                 block_seq.append(BlockNode(child))
             else:
                 get_blocks_v1(child, block_seq)
@@ -61,20 +80,18 @@ def get_blocks_v1(node, block_seq):
         block_seq.append(BlockNode(node))
         for child in children[1:]:
             token = get_token(child)
-            if not hasattr(node, 'block') and token not in logic+['BlockStatement']:
+            if not hasattr(node, "block") and token not in logic + ["BlockStatement"]:
                 block_seq.append(BlockNode(child))
             else:
                 get_blocks_v1(child, block_seq)
-            block_seq.append(BlockNode('End'))
-    elif name is 'BlockStatement' or hasattr(node, 'block'):
+            block_seq.append(BlockNode("End"))
+    elif name is "BlockStatement" or hasattr(node, "block"):
         block_seq.append(BlockNode(name))
         for child in children:
-            if get_token(child)not in logic:
+            if get_token(child) not in logic:
                 block_seq.append(BlockNode(child))
             else:
                 get_blocks_v1(child, block_seq)
     else:
         for child in children:
             get_blocks_v1(child, block_seq)
-
-
