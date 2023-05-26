2b49084554885303dbffa97bf2b883968f8f2d25
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-05-26 19:21:03 -0400

    ADDED code_to_repr:
    - script for processing inputs and getting the d,c,u tuple for each input

--- src/ast_nn/src/code_to_repr.py
+++ src/ast_nn/src/code_to_repr.py
@@ -1,6 +1,14 @@
 import networkx as nx
 import javalang
 from javalang.ast import Node
+import numpy as np
+from gensim.models.word2vec import Word2Vec
+
+word2vec = Word2Vec.load(
+    "/Users/ahura/Nexus/Leto/src/ast_nn/dataset/java/embeddings/node_w2v_128"
+).wv
+vocab = word2vec.vocab
+max_token = word2vec.syn0.shape[0]
 
 
 def code_to_ast(code: str, language: str):
@@ -19,22 +27,20 @@ def code_to_ast(code: str, language: str):
     return tree
 
 
-def ast_to_index(word2vec_path, tree, language):
-    from gensim.models.word2vec import Word2Vec
-
+def ast_to_index(tree, language):
     if language == "c":
         from prepare_data import get_blocks as func
     elif language == "java":
-        from utils import get_blocks_v1 as func
-
-    word2vec = Word2Vec.load(word2vec_path).wv
-    vocab = word2vec.vocab
-    max_token = word2vec.syn0.shape[0]
+        try:
+            from utils import get_blocks_v1 as func
+        except ImportError:
+            from ast_nn.src.utils import get_blocks_v1 as func
 
     def tree_to_index(node):
         token = node.token
-        result = [vocab[token].index if token in vocab else max_token]
         children = node.children
+
+        result = [vocab[token].index if token in vocab else max_token]
         for child in children:
             result.append(tree_to_index(child))
         return result
@@ -43,16 +49,52 @@ def ast_to_index(word2vec_path, tree, language):
         blocks = []
         func(r, blocks)
         tree = []
-        for b in blocks:
+        tree_info = {}
+
+        # for unpacking the list of children
+        def unpack(l):
+            for el in l:
+                if isinstance(el, list):
+                    yield from unpack(el)
+                else:
+                    yield el
+
+        for i, b in enumerate(blocks):
+            block_token = b.token
+            block_position = i + 1
+
             btree = tree_to_index(b)
-            tree.append(btree)
-        return tree
+            tree_info[i] = {
+                "d": block_position,
+                "u": vocab[block_token].index if block_token in vocab else max_token,
+                "c": list(unpack(btree)),
+            }
+        # unpakc all ds into a list
+        ds = [tree_info[i]["d"] for i in range(len(tree_info))]
+        cs = [tree_info[i]["c"] for i in range(len(tree_info))]
+        us = [tree_info[i]["u"] for i in range(len(tree_info))]
+        return {"d": ds, "c": cs, "u": us}
 
     return trans2seq(tree)
 
 
+def code_to_index(code, language):
+    tree = code_to_ast(code, language)
+    return ast_to_index(tree, language)
 
-tree = code_to_ast(
-    "public String getValue(String key) {\n        KeyValue kv = getKV(key);\n        return kv == null ? null : kv.getValue();\n    }",
-    "java",
-)
+
+if __name__ == "__main__":
+    # tree = code_to_ast(
+    #     "public static void pipe(InputStream in, OutputStream out) throws IOException {\n        byte[] buf = new byte[4096];\n        int n;\n        while ((n = in.read(buf, 0, buf.length)) >= 0) out.write(buf, 0, n);\n    }\n",
+    #     "java",
+    # )
+    # y = ast_to_index(
+    #     "/Users/ahura/Nexus/Leto/src/ast_nn/dataset/java/embeddings/node_w2v_128",
+    #     tree,
+    #     "java",
+    # )
+    x = code_to_index(
+        "public static void pipe(InputStream in, OutputStream out) throws IOException {\n        byte[] buf = new byte[4096];\n        int n;\n        while ((n = in.read(buf, 0, buf.length)) >= 0) out.write(buf, 0, n);\n    }\n",
+        "java",
+    )
+    print("i")
