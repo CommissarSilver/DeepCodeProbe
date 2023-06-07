680f9e3b8f6936daa6ad97662fc969621d012170
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-06-07 09:23:32 -0400

    MODIFIED probe_model.py:
    - added imports required for FuncGNN
    - collator function now needs to be sent as an input for the train_probe function
    - Added the probing for FunCGNN
    - The (d,c,u) tuple for FuncGNN is now done
    - FuncGNN has its own ParserProbe

--- src/probe_model.py
+++ src/probe_model.py
@@ -3,8 +3,15 @@ import numpy as np
 
 from datasets import load_dataset
 from utils import probe_utils
-from ast_probe.probe.utils import collator_fn
-from ast_probe.probe import ParserProbe, ParserLoss, get_embeddings
+from ast_probe.probe.utils import collator_fn_astnn, collator_fn_funcgnn
+from ast_probe.probe import (
+    ParserProbe,
+    ParserLossFuncGNN,
+    ParserLoss,
+    get_embeddings_astnn,
+    get_embeddings_funcgnn,
+    FuncGNNParserProbe,
+)
 
 warnings.filterwarnings("ignore")
 with open(os.path.join(os.getcwd(), "src", "logging_config.yaml"), "r") as f:
@@ -14,9 +21,11 @@ logging.config.dictConfig(config)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model_name = "astnn"
+# model_name = "astnn"
+model_name = "funcgnn"
 language = "java"
-dataset_path = "/Users/ahura/Nexus/Leto/dataset/ast-nn"
+# dataset_path = "/Users/ahura/Nexus/Leto/dataset/ast-nn"
+dataset_path = "/Users/ahura/Nexus/Leto/src/funcgnn/dataset"
 
 train_epochs = 20
 batch_size = 32
@@ -31,6 +40,7 @@ if model_name == "astnn":
     from ast_nn.src.model import BatchProgramCC
     from gensim.models.word2vec import Word2Vec
     from ast_probe.probe import get_embeddings
+    from utils.probe_utils import collator_fn_astnn
 
     data_files = {
         "train": os.path.join(dataset_path, "train.jsonl"),
@@ -149,7 +159,8 @@ if model_name == "astnn":
     ).to(device)
 
     probe_utils.train_probe(
-        embedding_func=get_embeddings,
+        embedding_func=get_embeddings_astnn,
+        collator_fn=collator_fn_astnn,
         train_dataset=train_set,
         valid_dataset=valid_set,
         batch_size=batch_size,
@@ -162,3 +173,61 @@ if model_name == "astnn":
     )
 
     print("Done!")
+
+elif model_name == "funcgnn":
+    from funcgnn.src.code_to_repr import code_to_index
+    from funcgnn.src.funcgnn import funcGNNTrainer
+    from funcgnn.src.param_parser import parameter_parser
+
+    data_files = {
+        "train": [
+            os.path.join(dataset_path, "train", i)
+            for i in os.listdir(os.path.join(dataset_path, "train"))
+        ],
+        "test": [
+            os.path.join(dataset_path, "test", i)
+            for i in os.listdir(os.path.join(dataset_path, "test"))
+        ],
+    }
+
+    train_set = load_dataset("json", data_files=data_files, split="train")
+    test_set = load_dataset("json", data_files=data_files, split="test")
+
+    train_set = train_set.map(lambda e: code_to_index(e))
+    test_set = test_set.map(lambda e: code_to_index(e))
+
+    max_d_len_train = max([len(x) for x in train_set["d"]])
+    max_d_len_test = max([len(x) for x in test_set["d"]])
+
+    max_c_len_train = max([len(x) for x in train_set["c"]])
+    max_c_len_test = max([len(x) for x in test_set["c"]])
+
+    max_u_len_train = max([len(x) for x in train_set["u"]])
+    max_u_len_test = max([len(x) for x in test_set["u"]])
+
+    funcgnn_param_parser = parameter_parser()
+    funcgnn_trainer = funcGNNTrainer(funcgnn_param_parser)
+    model_to_probe = funcgnn_trainer.model
+    # embedding_func = get_embeddings(model_name="funcgnn", model=model_to_probe,all_inputs=)
+    probe_model = FuncGNNParserProbe(
+        probe_rank=probe_rank,
+        hidden_dim=64,
+        number_labels_d=max_d_len_train,
+        number_labels_c=max_c_len_train,
+        number_labels_u=max_u_len_train,
+    ).to(device)
+
+    probe_utils.train_probe(
+        embedding_func=get_embeddings_funcgnn,
+        collator_fn=collator_fn_funcgnn,
+        train_dataset=train_set,
+        valid_dataset=test_set,
+        batch_size=batch_size,
+        patience=patience,
+        probe_model=probe_model,
+        probe_loss=ParserLossFuncGNN(max_c_len=max_c_len_train),
+        model_under_probe=model_to_probe,
+        train_epochs=train_epochs,
+        output_path=os.path.join(os.getcwd(), "results", "funcgnn"),
+    )
+    print("hi")
