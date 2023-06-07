2d222a9d724a769c40ace50099beabdfe4fc9d8f
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-06-07 10:08:14 -0400

    MODIFIED porbe.py:
    - documentation

--- src/ast_probe/probe/probe.py
+++ src/ast_probe/probe/probe.py
@@ -14,25 +14,49 @@ class Probe(nn.Module):
 
 
 class ParserProbe(Probe):
+    """
+    Probe for ASTNN
+    """
+
     def __init__(
-        self, probe_rank, hidden_dim, number_labels_d, number_labels_c, number_labels_u
+        self,
+        probe_rank,
+        hidden_dim,
+        number_labels_d,
+        number_labels_c,
+        number_labels_u,
     ):
+        """
+        Probe for ASTNN
+
+        Args:
+            probe_rank (_type_): rank of the probe. this is the number of vectors that will be used to project the word representations.
+            hidden_dim (_type_): hidden_dimnesion of the original model which is being probed.
+            number_labels_d (_type_): number of ds
+            number_labels_c (_type_): number of cs
+            number_labels_u (_type_): number of us
+        """
         print("Constructing ParserProbe")
         super(ParserProbe, self).__init__()
         self.probe_rank = probe_rank
         self.hidden_dim = hidden_dim
+
         self.number_vectors_d = number_labels_d
         self.number_vectors_c = number_labels_c
         self.number_vectors_u = number_labels_u
+        # first projection. aim here is to project the intermediate inputs to the probe rank
         self.proj = nn.Parameter(data=torch.zeros(self.hidden_dim, self.probe_rank))
         nn.init.uniform_(self.proj, -0.05, 0.05)
         #! add self.vectors_d for distance here later
+        # second projection exclusive to the ds
         self.vectors_d = nn.Parameter(data=torch.zeros(self.probe_rank))
         nn.init.uniform_(self.vectors_d, -0.05, 0.05)
+        # third projections to be used for the cs
         self.vectors_c = nn.Parameter(
             data=torch.zeros(self.probe_rank, self.number_vectors_c)
         )
         nn.init.uniform_(self.vectors_c, -0.05, 0.05)
+        # fourth projection to be used for us
         self.vectors_u = nn.Parameter(data=torch.zeros(self.probe_rank))
         nn.init.uniform_(self.vectors_u, -0.05, 0.05)
 
@@ -57,27 +81,46 @@ class ParserProbe(Probe):
 
 
 class FuncGNNParserProbe(Probe):
+    """
+    Probe for FuncGNN
+    """
+
     def __init__(
         self, probe_rank, hidden_dim, number_labels_d, number_labels_c, number_labels_u
     ):
+        """
+        Probe for FuncGNN
+
+        Args:
+            probe_rank (_type_): rank of the probe. this is the number of vectors that will be used to project the word representations.
+            hidden_dim (_type_): hidden_dimnesion of the original model which is being probed.
+            number_labels_d (_type_): number of ds
+            number_labels_c (_type_): number of cs
+            number_labels_u (_type_): number of us
+        """
         print("Constructing ParserProbe")
         super(FuncGNNParserProbe, self).__init__()
         self.probe_rank = probe_rank
         self.hidden_dim = hidden_dim
+
         self.number_vectors_d = number_labels_d
         self.number_vectors_c = number_labels_c
         self.number_vectors_u = number_labels_u
+        # first projection. aim here is to project the intermediate inputs to the probe rank
         self.proj = nn.Parameter(data=torch.zeros(self.hidden_dim, self.probe_rank))
         nn.init.uniform_(self.proj, -0.05, 0.05)
-        #! add self.vectors_d for distance here later
+        # second projection exclusive to the ds
         self.vectors_d = nn.Parameter(data=torch.zeros(self.probe_rank))
         nn.init.uniform_(self.vectors_d, -0.05, 0.05)
+        # third projections to be used for the cs
         self.vectors_c = nn.Parameter(data=torch.zeros(self.probe_rank, 1))
         nn.init.uniform_(self.vectors_c, -0.05, 0.05)
+        # c vector prediction for FuncGNN requires a second projection after the first projection
         self.vectors_c_edges = nn.Parameter(
             data=torch.zeros(self.number_vectors_d, self.number_vectors_c * 2)
         )
         nn.init.uniform_(self.vectors_c_edges, -0.05, 0.05)
+        # fourth projection to be used for us
         self.vectors_u = nn.Parameter(data=torch.zeros(self.probe_rank))
         nn.init.uniform_(self.vectors_u, -0.05, 0.05)
 
@@ -93,13 +136,14 @@ class FuncGNNParserProbe(Probe):
             scores_u: (batch_size, max_seq_len, number classes_u)
         """
         transformed = torch.matmul(batch, self.proj)
-        # j = transformed.unsqueeze(2)
+
         ds_pred = torch.matmul(transformed, self.vectors_d)
+
         cs_pred_proj = torch.matmul(transformed, self.vectors_c)
-        # j = torch.squeeze(cs_pred_proj)
         cs_pred = torch.matmul(torch.squeeze(cs_pred_proj), self.vectors_c_edges).view(
             cs_pred_proj.shape[0], 64, 2
         )
+
         us_pred = torch.matmul(transformed, self.vectors_u)
 
         return (ds_pred, cs_pred, us_pred)
