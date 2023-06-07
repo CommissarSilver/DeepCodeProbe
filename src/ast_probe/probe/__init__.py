fa02f758e1497d2a8ff5885378c7a7772d96189e
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-06-07 09:24:15 -0400

    MODIFIED probe/init.py: - Added the ParserLoss, ParserProbe, and embedding function of FuncGNN to the imports

--- src/ast_probe/probe/__init__.py
+++ src/ast_probe/probe/__init__.py
@@ -1,3 +1,3 @@
-from .loss import ParserLoss
-from .probe import ParserProbe
-from .utils import get_embeddings
+from .loss import ParserLoss, ParserLossFuncGNN
+from .probe import ParserProbe, FuncGNNParserProbe
+from .utils import get_embeddings_astnn, get_embeddings_funcgnn
