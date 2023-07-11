15849133e4537da8b5d6dc88121f8d920792e830
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-07-11 10:33:01 -0400

    MODIFIED src/summarization_tf/src/code_to_repr.py:
    - cahnged it from processing the netire data to processing single instances so that it adheres to the style of other projects

--- src/summarization_tf/src/code_to_repr.py
+++ src/summarization_tf/src/code_to_repr.py
@@ -1,30 +1,58 @@
 import javalang, json
 
+u_index = {}
+u_index_counter = 0
 
-def code_to_ast(path_to_code: str):
-    all = open(path_to_code).readlines()
+
+def code_to_ast(code_json: str):
     num_faulty_codes = 0
-    for i in all:
-        json_data = json.loads(i)["code"]
-        try:
-            tokens = javalang.tokenizer.tokenize(json_data)
-            parser = javalang.parser.Parser(tokens)
+    D = []
+    C = []
+    U = []
 
-            tree = parser.parse_member_declaration()
+    json_data = json.loads(code_json)["code"]
+    try:
+        tokens = javalang.tokenizer.tokenize(json_data)
+        parser = javalang.parser.Parser(tokens)
 
-            node_names = [node.__class__.__name__ for path, node in tree]
-            node_children = [len(node.children) for path, node in tree]
-            node_positions = [node.position for path, node in tree]
-        except Exception as e:
-            print(e)
-            num_faulty_codes += 1
-            continue
-    print(num_faulty_codes)
+        tree = parser.parse_member_declaration()
+
+        node_names = [node.__class__.__name__ for path, node in tree]
+        node_children = [len(node.children) for path, node in tree]
+        node_positions = [
+            (node.position.line, node.position.column)
+            for path, node in tree
+            if node.position is not None
+        ]
+        D = node_positions
+        C = node_children
+        U = node_names
+    except Exception as e:
+        pass
+
+    return D, C, U
 
 
-def ast_to_index():
-    pass
+def ast_to_index(D, C, U):
+    final_d = []
+    final_c = []
+    final_u = []
+    global u_index, u_index_counter
+
+    for label in U:
+        if label not in u_index.keys():
+            u_index[label] = u_index_counter
+            u_index_counter += 1
+    final_d = D
+    final_c = C
+    final_u = [u_index[label] for label in U]
+
+    return final_d, final_c, final_u
 
 
 if __name__ == "__main__":
-    code_to_ast("/Users/ahura/Nexus/Leto/src/summarization_tf/dataset/test.json")
+    path_to_code = "/Users/ahura/Nexus/Leto/src/summarization_tf/dataset/valid.json"
+    codes = open(path_to_code).readlines()
+    for code in codes:
+        ds, cs, us = code_to_ast(code)
+        ds, cs, us = ast_to_index(ds, cs, us)
