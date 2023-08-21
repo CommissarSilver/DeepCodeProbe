62a0c1f80d768efac73a6e06339c699bdc4b19ea
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-08-21 09:44:32 -0400

    MODIFIED ast_nn/src/code_to_repr:
    - updated the ast_to_index function to conform to the new libraries

--- src/ast_nn/src/code_to_repr.py
+++ src/ast_nn/src/code_to_repr.py
@@ -4,12 +4,6 @@ from javalang.ast import Node
 import numpy as np
 from gensim.models.word2vec import Word2Vec
 
-word2vec = Word2Vec.load(
-    "/Users/ahura/Nexus/Leto/src/ast_nn/dataset/java/embeddings/node_w2v_128"
-).wv
-vocab = word2vec.vocab
-max_token = word2vec.syn0.shape[0]
-
 
 def code_to_ast(code: str, language: str):
     if language == "c":
@@ -30,9 +24,21 @@ def code_to_ast(code: str, language: str):
 def ast_to_index(tree, language):
     if language == "c":
         from prepare_data import get_blocks as func
+
+        word2vec = Word2Vec.load(
+            "/Users/ahura/Nexus/Leto/src/ast_nn/dataset/c/embeddings/node_w2v_128"
+        ).wv
+        vocab = word2vec
+        max_token = word2vec.vectors.shape[0]
     elif language == "java":
         try:
             from utils import get_blocks_v1 as func
+
+            word2vec = Word2Vec.load(
+                "/Users/ahura/Nexus/Leto/src/ast_nn/dataset/java/embeddings/node_w2v_128"
+            ).wv
+            vocab = word2vec
+            max_token = word2vec.vectors.shape[0]
         except ImportError:
             from ast_nn.src.utils import get_blocks_v1 as func
 
@@ -40,7 +46,7 @@ def ast_to_index(tree, language):
         token = node.token
         children = node.children
 
-        result = [vocab[token].index if token in vocab else max_token]
+        result = [vocab.key_to_index[token] if token in vocab else max_token]
         for child in children:
             result.append(tree_to_index(child))
         return result
@@ -66,14 +72,14 @@ def ast_to_index(tree, language):
             btree = tree_to_index(b)
             tree_info[i] = {
                 "d": block_position,
-                "u": vocab[block_token].index if block_token in vocab else max_token,
+                "u": vocab.key_to_index[block_token] if block_token in vocab else max_token,
                 "c": list(unpack(btree)),
             }
         # unpakc all ds into a list
         ds = [tree_info[i]["d"] for i in range(len(tree_info))]
         cs = [tree_info[i]["c"] for i in range(len(tree_info))]
         us = [tree_info[i]["u"] for i in range(len(tree_info))]
-        
+
         return {"d": ds, "c": cs, "u": us}
 
     return trans2seq(tree)
@@ -98,4 +104,4 @@ if __name__ == "__main__":
         "public static void pipe(InputStream in, OutputStream out) throws IOException {\n        byte[] buf = new byte[4096];\n        int n;\n        while ((n = in.read(buf, 0, buf.length)) >= 0) out.write(buf, 0, n);\n    }\n",
         "java",
     )
-    print("i")
+    print(x)
