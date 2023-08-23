707b7986a91dd735d9786926213448a2d43418cd
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-08-23 18:02:51 -0400

    MODIFFIED code_sum_drl/lib/model/Generator.py:
    - changes to make it work with new version of torch and python3

--- src/code_sum_drl/src/lib/model/Generator.py
+++ src/code_sum_drl/src/lib/model/Generator.py
@@ -13,14 +13,18 @@ class BaseGenerator(nn.Module):
     def forward(self, inputs):
         return self.generator(inputs.contiguous().view(-1, inputs.size(-1)))
 
-    def backward(self, outputs, targets, weights, normalizer, criterion, regression=False):
+    def backward(
+        self, outputs, targets, weights, normalizer, criterion, regression=False
+    ):
         outputs = Variable(outputs.data, requires_grad=True)
 
         logits = outputs.contiguous().view(-1) if regression else self.forward(outputs)
 
-        loss = criterion(logits, targets.contiguous().view(-1), weights.contiguous().view(-1))
+        loss = criterion(
+            logits, targets.contiguous().view(-1), weights.contiguous().view(-1)
+        )
         loss.div(normalizer).backward()
-        loss = loss.data[0]
+        loss = loss.item()
 
         if outputs.grad is None:
             grad_output = torch.zeros(outputs.size())
@@ -33,7 +37,9 @@ class BaseGenerator(nn.Module):
         logits = self.forward(outputs)
         preds = logits.data.max(1)[1].view(outputs.size(0), -1)
 
-        loss = criterion(logits, targets.contiguous().view(-1), weights.contiguous().view(-1)).data[0]
+        loss = criterion(
+            logits, targets.contiguous().view(-1), weights.contiguous().view(-1)
+        ).item()
 
         return preds, loss
 
@@ -44,7 +50,9 @@ class MemEfficientGenerator(BaseGenerator):
         self.batch_size = opt.max_generator_batches
         self.dim = dim
 
-    def backward(self, outputs, targets, weights, normalizer, criterion, regression=False):
+    def backward(
+        self, outputs, targets, weights, normalizer, criterion, regression=False
+    ):
         outputs_split = torch.split(outputs, self.batch_size, self.dim)
         targets_split = torch.split(targets, self.batch_size, self.dim)
         weights_split = torch.split(weights, self.batch_size, self.dim)
@@ -53,7 +61,8 @@ class MemEfficientGenerator(BaseGenerator):
         loss = 0
         for out_t, targ_t, w_t in zip(outputs_split, targets_split, weights_split):
             grad_output_t, loss_t = super(MemEfficientGenerator, self).backward(
-                out_t, targ_t, w_t, normalizer, criterion, regression)
+                out_t, targ_t, w_t, normalizer, criterion, regression
+            )
             grad_output.append(grad_output_t)
             loss += loss_t
 
@@ -69,11 +78,10 @@ class MemEfficientGenerator(BaseGenerator):
         loss = 0
         for out_t, targ_t, w_t in zip(outputs_split, targets_split, weights_split):
             preds_t, loss_t = super(MemEfficientGenerator, self).predict(
-                out_t, targ_t, w_t, criterion)
+                out_t, targ_t, w_t, criterion
+            )
             preds.append(preds_t)
             loss += loss_t
 
         preds = torch.cat(preds, self.dim)
         return preds, loss
-
-
