5d796ff1eb7ffb19931fa392bc37064f757e7dd2
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-08-31 19:10:18 -0400

    MODIFIED ast_probe/probe/__init__:
    - added the functions import for sum_tf probing

--- src/ast_probe/probe/__init__.py
+++ src/ast_probe/probe/__init__.py
@@ -1,3 +1,9 @@
 from .loss import ParserLoss, ParserLossFuncGNN
 from .probe import ParserProbe, FuncGNNParserProbe
-from .utils import get_embeddings_astnn, get_embeddings_funcgnn, collator_fn_astnn
+from .utils import (
+    get_embeddings_astnn,
+    get_embeddings_funcgnn,
+    collator_fn_astnn,
+    get_embeddings_sum_tf,
+    collator_fn_sum_tf,
+)
