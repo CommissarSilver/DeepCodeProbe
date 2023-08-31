07e2cda62207a77f5a561057b136fd3a07fedce8
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-08-31 19:09:44 -0400

    MODIFIED src -> probe_model:
    - Added the preparation phase for probing sum_tf
    - Unfortunately, because of the fomat of the underlying data, we needed to do some patch jobs which ain't pretty

--- src/probe_model.py
+++ src/probe_model.py
@@ -34,13 +34,13 @@ parser.add_argument(
     type=str,
     help="Model to probe",
     choices=["ast_nn", "funcgnn", "code_sum_drl", "sum_tf", "cscg_dual"],
-    default="funcgnn",
+    default="sum_tf",
 )
 parser.add_argument(
     "--dataset_path",
     type=str,
     help="Path to the dataset - Path follows the format /model_name/dataset",
-    default=os.path.join(os.getcwd(), "src", "funcgnn", "dataset"),
+    default=os.path.join(os.getcwd(), "src", "summarization_tf", "dataset"),
 )
 parser.add_argument(
     "--language",
@@ -280,3 +280,119 @@ elif args.model == "funcgnn":
         output_path=os.path.join(os.getcwd(), "results", "funcgnn"),
     )
     print("hi")
+
+elif args.model == "sum_tf":
+    from summarization_tf.src.code_to_repr import code_to_index
+    from summarization_tf.src.models import MultiwayModel
+    import pandas as pd
+    from summarization_tf.src.utils import read_pickle
+    from ast_probe.probe import get_embeddings_sum_tf, collator_fn_sum_tf
+    from torch.utils.data import Dataset
+
+    class CustomDataset(Dataset):
+        def __init__(self, dataframe):
+            self.dataframe = dataframe
+
+        def __len__(self):
+            return len(self.dataframe)
+
+        def __getitem__(self, index):
+            return self.dataframe.iloc[index]
+
+    data_files = {
+        "train": os.path.join(args.dataset_path, "train.json"),
+        "valid": os.path.join(args.dataset_path, "valid.json"),
+        "test": os.path.join(args.dataset_path, "test.json"),
+    }
+
+    train_set = load_dataset("json", data_files=data_files, split="train[:64]")
+    valid_set = load_dataset("json", data_files=data_files, split="valid[:64]")
+    test_set = load_dataset("json", data_files=data_files, split="test[:64]")
+
+    train_set_processed = [
+        code_to_index(i["code"], i["nl"], idx) for idx, i in enumerate(train_set)
+    ]
+    valid_set_processed = [
+        code_to_index(i["code"], i["nl"], idx) for idx, i in enumerate(valid_set)
+    ]
+    test_set_processed = [
+        code_to_index(i["code"], i["nl"], idx) for idx, i in enumerate(test_set)
+    ]
+
+    train_set_processed = [i for i in train_set_processed if i is not None]
+    test_set_processed = [i for i in test_set_processed if i is not None]
+    valid_set_processed = [i for i in valid_set_processed if i is not None]
+    train_set = pd.DataFrame(train_set_processed)
+    valid_set = pd.DataFrame(valid_set_processed)
+    test_set = pd.DataFrame(test_set_processed)
+
+    max_d_len_train = max([len(x) for x in train_set["d"]])
+    max_c_len_train = max([len(x) for x in train_set["c"]])
+    max_u_len_train = max([len(x) for x in train_set["u"]])
+
+    max_d_len_valid = max([len(x) for x in valid_set["d"]])
+    max_c_len_valid = max([len(x) for x in valid_set["c"]])
+    max_u_len_valid = max([len(x) for x in valid_set["u"]])
+
+    max_d_len_test = max([len(x) for x in test_set["d"]])
+    max_c_len_test = max([len(x) for x in test_set["c"]])
+    max_u_len_test = max([len(x) for x in test_set["u"]])
+
+    max_d_len, max_c_len, max_u_len = (
+        max(
+            max_d_len_train,
+            max_d_len_valid,
+            max_d_len_test,
+        ),
+        max(
+            max_c_len_train,
+            max_c_len_valid,
+            max_c_len_test,
+        ),
+        max(
+            max_u_len_train,
+            max_u_len_valid,
+            max_u_len_test,
+        ),
+    )
+
+    code_w2i = read_pickle(f"{args.dataset_path}/code_w2i.pkl")
+    nl_w2i = read_pickle(f"{args.dataset_path}/nl_w2i.pkl")
+    model = MultiwayModel(
+        512,
+        512,
+        512,
+        len(code_w2i),
+        len(nl_w2i),
+        dropout=0.5,
+        lr=0.001,
+        layer=1,
+    )
+
+    train_set = CustomDataset(train_set)
+    test_set = CustomDataset(test_set)
+    valid_set = CustomDataset(valid_set)
+    probe_model = FuncGNNParserProbe(
+        probe_rank=args.probe_rank,
+        hidden_dim=64,
+        number_labels_d=max_d_len_train,
+        number_labels_c=max_c_len_train,
+        number_labels_u=max_u_len_train,
+    ).to(device)
+
+    probe_utils.train_probe(
+        embedding_func=get_embeddings_sum_tf,
+        collator_fn=collator_fn_sum_tf,
+        train_dataset=train_set,
+        valid_dataset=test_set,
+        batch_size=args.batch_size,
+        patience=args.patience,
+        probe_model=probe_model,
+        probe_loss=ParserLossFuncGNN(max_c_len=max_c_len_train),
+        model_under_probe=model,
+        train_epochs=args.train_epochs,
+        output_path=os.path.join(os.getcwd(), "results", "funcgnn"),
+    )
+    # first, we need to call each code instance and get the original repr of it to feed to the model
+    # second, for each of these call the encode function of the model to get the embeddings
+    # send them to the probe to be investigated
