0c85ac7a57bd552462fb6f30d77c86a8da957149
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-09-10 11:33:01 -0400

    MODIFIED ast_probe->probe->loss.py:
    - Added the loss function for codeSumDRL

--- src/ast_probe/probe/loss.py
+++ src/ast_probe/probe/loss.py
@@ -309,3 +309,99 @@ class ParserLossSumTF(nn.Module):
             (length_batch * d_pred.size(1)),
             (length_batch * c_pred.size(1) * c_pred.size(2)),
         )
+
+
+class ParserLossCodeSumDRL(nn.Module):
+    """
+    Loss function for CodeSumDRL
+    """
+
+    def __init__(self):
+        """
+        we're using L1Loss for calcualting the distances for each tuple element in the predictions
+
+        Args:
+            max_c_len (_type_): maximum number of cs in the dataset
+
+        """
+        super(ParserLossCodeSumDRL, self).__init__()
+        self.ds = nn.L1Loss()
+        self.cs = nn.L1Loss()
+        self.us = nn.L1Loss()
+
+    def forward(self, d_pred, c_pred, u_pred, d_real, c_real, u_real):
+        """
+        forward function for calculating the loss
+
+        Args:
+            d_pred (_type_): ds predicted by the model
+            c_pred (_type_): cs predicted by the model
+            u_pred (_type_): us predicted by the model
+            d_real (_type_): the real d to be used as ground truth
+            c_real (_type_): the real c to be used as ground truth
+            u_real (_type_): the real u to be used as ground truth
+            #!length_batch (_type_): i don't think this is used anymore. to be checked later
+
+        Returns:
+            the accumulated loss of d,c,u
+        """
+        # unlike ASTNN, there is a need to match the lengths of d_pred and d_real
+
+        d_real = torch.tensor([d + [-1] * (d_pred.shape[1] - len(d)) for d in d_real])
+        c_real = torch.tensor([c + [-1] * (c_pred.shape[1] - len(c)) for c in c_real])
+        u_real = torch.tensor([u + [-1] * (u_pred.shape[1] - len(u)) for u in u_real])
+
+        loss_d = self.ds(d_pred, d_real)
+        loss_c = self.cs(c_pred, c_real)
+        loss_u = self.us(u_pred, u_real)
+
+        return loss_d + loss_c + loss_u
+
+    @staticmethod
+    def calculate_hits(d_pred, c_pred, u_pred, d_real, c_real, u_real, length_batch):
+        """
+        calculates the number of correct predictions of the probe by comparing the predictions with the real values.
+        to calculate the number of hits I'm using round function. if the predicted value is 1.1 and the real value is 1, torch.round(1.1) will return 1.
+        Basically, torch.round the closest integer greater than or equal to each element.
+
+        Args:
+            d_pred (_type_): ds predicted by the model
+            c_pred (_type_): cs predicted by the model
+            u_pred (_type_): us predicted by the model
+            d_real (_type_): the real d to be used as ground truth
+            c_real (_type_): the real c to be used as ground truth
+            u_real (_type_): the real u to be used as ground truth
+            length_batch (_type_): _description_
+
+        Returns:
+            _type_: the number of correct predictions for d,c,u
+        """
+        if d_real.shape[1] != d_pred.shape[1]:
+            padding_size = d_pred.shape[1] - d_real.shape[1]
+            d_real = torch.nn.functional.pad(d_real, (0, padding_size), value=-1)
+
+        if c_pred.shape[1] != c_real.shape[1]:
+            num_elements = c_pred.shape[1] - c_real.shape[1]
+            fill_tensor = torch.full((c_real.size(0), num_elements, 2), -1)
+            c_real = torch.cat((c_real, fill_tensor), dim=1)
+
+        if u_real.shape[1] != u_pred.shape[1]:
+            padding_size = u_pred.shape[1] - u_real.shape[1]
+            u_real = torch.nn.functional.pad(u_real, (0, padding_size), value=-1)
+
+        d_hits = torch.round(d_pred).eq(d_real).sum().item()
+        u_hits = torch.round(u_pred).eq(u_real).sum().item()
+
+        if c_pred.shape[2] != c_real.shape[2]:
+            num_elements = c_pred.shape[2] - c_real.shape[2]
+            fill_tensor = torch.full((c_real.size(0), c_real.size(1), num_elements), -1)
+            c_real = torch.cat((c_real, fill_tensor), dim=2)
+        c_hits = torch.round(c_pred).eq(c_real).sum().item()
+
+        return (
+            d_hits,
+            c_hits,
+            u_hits,
+            (length_batch * d_pred.size(1)),
+            (length_batch * c_pred.size(1) * c_pred.size(2)),
+        )
