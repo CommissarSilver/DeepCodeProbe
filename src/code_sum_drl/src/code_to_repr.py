990ce7ba128d88ec508589ace50e127b30fdc69a
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-09-10 11:35:04 -0400

    MODIFIED code_sum_drl->src->code_to_repr:
    - Finalized the code to repr mechanism for codeSumDRL

--- src/code_sum_drl/src/code_to_repr.py
+++ src/code_sum_drl/src/code_to_repr.py
@@ -1,9 +1,126 @@
-import ast, re
+import ast, math, random, torch
+from torch.autograd import Variable
 
-import ast
+try:
+    import lib
+except ImportError:
+    import code_sum_drl.src.lib as lib
 
 u_index = {}
 u_index_counter = 0
+
+
+class Dataset(object):
+    def __init__(self, data, batchSize, cuda, eval=False):
+        self.src = data["src"]
+        self.tgt = data["tgt"]
+        self.trees = data["trees"]
+        self.leafs = data["leafs"]
+        self.original_codes = data["original_codes"]
+        self.original_comments = data["original_comments"]
+
+        assert len(self.src) == len(self.tgt)
+        self.cuda = cuda
+
+        self.batchSize = batchSize
+        self.numBatches = int(math.ceil(len(self.src) / batchSize) - 2)
+        self.eval = eval
+
+    def _batchify(self, data, align_right=False, include_lengths=False):
+        lengths = [x.size(0) for x in data]
+        max_length = max(lengths)
+        out = data[0].new(len(data), max_length).fill_(lib.Constants.PAD)
+        for i in range(len(data)):
+            data_length = data[i].size(0)
+            offset = max_length - data_length if align_right else 0
+            out[i].narrow(0, offset, data_length).copy_(data[i])
+
+        if include_lengths:
+            return out, lengths
+        else:
+            return out
+
+    def __getitem__(self, index):
+        assert index <= self.numBatches, "%d > %d" % (index, self.numBatches)
+        srcBatch, src_lengths = self._batchify(
+            self.src[index * self.batchSize : (index + 1) * self.batchSize],
+            include_lengths=True,
+        )
+
+        leafBatch, leaf_lengths = self._batchify(
+            self.leafs[index * self.batchSize : (index + 1) * self.batchSize],
+            include_lengths=True,
+        )
+        srcTrees = self.trees[index * self.batchSize : (index + 1) * self.batchSize]
+        tgtBatch = self._batchify(
+            self.tgt[index * self.batchSize : (index + 1) * self.batchSize]
+        )
+
+        srcCodes = self.original_codes[
+            index * self.batchSize : (index + 1) * self.batchSize
+        ]
+        srcComments = self.original_comments[
+            index * self.batchSize : (index + 1) * self.batchSize
+        ]
+        srcCodeIndexes = [code_to_index(code) for code in srcCodes]
+
+        indices = range(len(srcBatch))
+        src_batch = zip(
+            indices,
+            srcBatch,
+            leafBatch,
+            leaf_lengths,
+            srcTrees,
+            tgtBatch,
+            srcCodes,
+            srcComments,
+            srcCodeIndexes,
+        )
+
+        src_batch, src_lengths = zip(
+            *sorted(zip(src_batch, src_lengths), key=lambda x: -x[1])
+        )
+
+        (
+            indices,
+            srcBatch,
+            leafBatch,
+            leaf_lengths,
+            srcTrees,
+            tgtBatch,
+            srcCodes,
+            srcComments,
+            srcCodeIndexes,
+        ) = zip(*src_batch)
+
+        tree_lengths = []
+        for tree in srcTrees:
+            l_c = tree.leaf_count()
+            tree_lengths.append(l_c)
+
+        def wrap(b):
+            b = torch.stack(b, 0).t().contiguous()
+            if self.cuda:
+                b = b.cuda()
+            if self.eval:
+                with torch.no_grad():
+                    b = Variable(b)
+            else:
+                b = Variable(b)
+            return b
+
+        return (
+            (wrap(srcBatch), src_lengths),
+            (srcTrees, tree_lengths, (wrap(leafBatch), leaf_lengths)),
+            wrap(tgtBatch),
+            indices,
+            srcCodes,
+            srcComments,
+            srcCodeIndexes,
+        )
+
+    def __len__(self):
+        return self.numBatches - 1
 
 
 def extract_node_info(node, parent=None, position=0):
@@ -102,10 +219,12 @@ def ast_to_index(
 
 def code_to_index(code: str):
     D, C, U = code_to_ast(code)
+
     if (D, C, U) == ([], [], []):
         return {"d": [], "c": [], "u": []}
     else:
         D, C, U = ast_to_index(D, C, U)
+        # fill the rest with 0 if the length is less than 100
         return {"d": D, "c": C, "u": U}
 
 
