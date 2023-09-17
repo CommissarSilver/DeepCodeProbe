7a5b5b0018a8b316a25c14f6e0a715ff2fdbe6ba
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-09-17 17:27:28 -0400

    unimportant changes

--- src/ast_probe/probe/__init__.py
+++ src/ast_probe/probe/__init__.py
@@ -4,6 +4,7 @@ from .utils import (
     get_embeddings_astnn,
     get_embeddings_funcgnn,
     collator_fn_astnn,
+    collator_fn_funcgnn,
     get_embeddings_sum_tf,
     collator_fn_sum_tf,
     get_embeddings_code_sum_drl,
