74fd8a3a103ba497fbb741ea744ae33839221065
CommissarSilver <a.majdinasab@hotmail.com>
2023-11-23 08:14:05 -0500

    Update language variables in pretrain.sh script for CSCG

--- src/cscg_dual/src/pretrain.sh
+++ src/cscg_dual/src/pretrain.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-L1='code'
-L2='nl'
+L1='nl'
+L2='code'
 JOB='pretrain'
 
-data_dir="/Users/ahura/Nexus/CSCGDual/data/python/original"
+data_dir="/store/travail/vamaj/Leto/src/cscg_dual/dataset/python/original"
 vocab_bin="$data_dir/vocab.bin"
 train_src="$data_dir/train.token.${L1}"
 train_tgt="$data_dir/train.token.${L2}"
@@ -12,9 +12,12 @@ test_src="$data_dir/valid.token.${L1}"
 test_tgt="$data_dir/valid.token.${L2}"
 
 job_name="$JOB"
-model_name="/store/travail/vamaj/CSCGDual/pretrain_models/c2nl.${job_name}"
+model_name="/store/travail/vamaj/Leto/src/cscg_dual/pretrain_models/nl2c.${job_name}"
 
 python nmt/nmt.py \
+    --cuda \
+    --gpu 0 \
+    --load_model /store/travail/vamaj/Leto/src/cscg_dual/pretrain_models/nl2c.pretrain.iter12000.bin \
     --mode train \
     --vocab ${vocab_bin} \
     --save_to ${model_name} \
@@ -23,7 +26,7 @@ python nmt/nmt.py \
     --valid_metric bleu \
     --save_model_after 2 \
     --beam_size 10 \
-    --batch_size 16 \
+    --batch_size 4 \
     --hidden_size 512 \
     --embed_size 512 \
     --uniform_init 0.1 \
@@ -36,4 +39,4 @@ python nmt/nmt.py \
     --train_tgt ${train_tgt} \
     --dev_src ${test_src} \
     --dev_tgt ${test_tgt}
-    
+
